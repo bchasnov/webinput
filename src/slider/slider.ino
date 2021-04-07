487881166f65644545b3880def01fdb096cb98ef
Ben Chasnov <bchasnov@uw.edu>
2021-04-07 12:42:41 -0700

    reftracking

---
+++ src/slider/slider.ino
@@ -0,0 +1,14 @@
+/*  
+* ESP32 LED Blink Example  
+*/  
+#define LED 2  
+void setup() {  
+ // Set pin mode  
+ pinMode(LED,OUTPUT);  
+}  
+void loop() {  
+ delay(500); // 500ms  
+ digitalWrite(LED,HIGH); // Turn on LED  
+ delay(500); // 500ms   
+ digitalWrite(LED,LOW); // Turn off LED  
+}
--- src/webinput/webinput.py
+++ src/webinput/webinput.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
-
-# WS server that sends messages at random intervals
-
 import asyncio
 import websockets
+import sys
 
 import math
 import time
 import json
-import pyserial
+import serial
 
-
-def server(freq: float):
+def server(freq: float, device):
     """Returns a websocket handler.
 
     Input: freq
@@ -20,20 +17,42 @@ def server(freq: float):
 
     async def handler(websocket, path):
         while True:
-            x = math.sin(time.time())
+            if device is not None:
+                x = device.grabData()[1]
+            else:
+                x = time.time() % 100 - 50
             package = dict(I=dict(x=x))
 
             await websocket.send(json.dumps(package))
             await asyncio.sleep(1./freq)
     return handler
 
+def setup(port):
+    from slider import sliderUSB as slider
+    try:
+        device = slider(port=port)
+    except:
+        print('No device detected at port: {}'.format(port))
+        sys.exit(0)
+
+    device.startArduino()
+
+    return device
+
+
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=None, type=str)
     parser.add_argument('--freq', default=60, type=float)
     args = parser.parse_args()
 
-    start_server = websockets.serve(server(args.freq), "localhost", 8765)
+    device = None
+    if args.port is not None:
+        device = setup(args.port)
+
+    start_server = websockets.serve(server(args.freq, device), "localhost", 8765)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
+
